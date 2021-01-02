0c827cbb71193a9c7073ba70457776e4985c3209
The Mindful Creator <themindfulcreator@gmail.com>
2021-01-03 01:24:15 +0800

    Initiated SIT Project
    Initiated SIT Project for its official website.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be the official website of SMOHRZ International Trading.
+        </p>
+        <p>
+          Stay tuned for the latest update.
         </p>
         <a
           className="App-link"
